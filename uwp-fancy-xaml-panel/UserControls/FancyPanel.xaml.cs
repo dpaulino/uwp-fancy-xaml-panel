ddf957ad53c14e8e0abb53ad50880a72325eae15
Lance McCarthy <lance.mccarthy@progress.com>
2018-12-14 17:01:33 -0500

    Gave FancyPanel a "PanelContent" property for better reusability

--- uwp-fancy-xaml-panel/UserControls/FancyPanel.xaml.cs
+++ uwp-fancy-xaml-panel/UserControls/FancyPanel.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace uwp_fancy_xaml_panel.UserControls
 {
@@ -23,9 +11,26 @@ namespace uwp_fancy_xaml_panel.UserControls
 
         public FancyPanel()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         private void Backdrop_Tapped(object sender, TappedRoutedEventArgs e) => BackdropTapped?.Invoke(this, new EventArgs());
+
+        public static readonly DependencyProperty PanelContentProperty = DependencyProperty.Register(
+            "PanelContent", typeof(FrameworkElement), typeof(FancyPanel), new PropertyMetadata(default(FrameworkElement), OnPanelContentChanged));
+
+        private static void OnPanelContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FancyPanel instance)
+            {
+                instance.Presenter.Content = e.NewValue;
+            }
+        }
+
+        public FrameworkElement PanelContent
+        {
+            get => (FrameworkElement) GetValue(PanelContentProperty);
+            set => SetValue(PanelContentProperty, value);
+        }
     }
 }
